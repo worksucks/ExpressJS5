6455e96fbe9e47f255250d01bfa41f1998d5d83b
Tomasz Reizer <t_reizer@yahoo.com>
2019-07-28 15:47:13 +0200

    added congig.js server.js and catalog  views

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '732787301490-pfo35kt4l9vm7cr9e46db6u0q8lh7g97.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'Z-bPyD7qG2JShyDF9eA3nnv3',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
